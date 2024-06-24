dbcc1f83a556526d43781d3a3ffceeb6fa06cad6
mrampai023 <mrampai023@student.wethinkcode.co.za>
2024-06-24 16:00:04 +0200

    added client commands in simpleClient

--- robot_combat/src/main/java/com/example/client/SimpleClient.java
+++ robot_combat/src/main/java/com/example/client/SimpleClient.java
@@ -58,6 +58,98 @@ public class SimpleClient {
                             System.out.println("World has visibility of "+ data.path("visibility") + " steps, reload time of "+ data.path("reload")+ "s and repair time of "+ data.path("repair")+"s.");
                         }
                     }
+                    case ("forward"), ("back") -> {
+                        String message = serverResponse.path("data").path("message").asText();
+                        switch (message) {
+                            case "FAILED_PITFALL" -> System.out.println(robotName + " has fallen into a pit");
+                            case "FAILED_OUTSIDE_WORLD" -> System.out.println(robotName + " cannot go out of bounds");
+                            case "FAILED_OBSTRUCTED_OBSTACLE" -> System.out.println(robotName + " is obstructed by an obstacle");
+                            case "FAILED_OBSTRUCTED_ROBOT" -> System.out.println(robotName + " is obstructed by a robot");
+                            case "RELOAD" -> System.out.println(robotName + " is currently reloading");
+                            case "REPAIR" -> System.out.println(robotName + " is currently repairing its shield");
+                            case "DEAD" -> System.out.println(robotName + " is dead");
+                            case "Could not parse arguments" -> System.out.println("could not parse arguments");
+                            default -> System.out.println(robotName + ": moved " + instruction[0] + " - now at position " + serverResponse.path("state").path("position"));
+                        }
+                    }
+                    case ("turn") -> {
+                        String message = serverResponse.path("data").path("message").asText();
+                        switch (message){
+                            case "RELOAD" -> System.out.println(robotName + " is currently reloading");
+                            case "REPAIR" -> System.out.println(robotName + " is currently repairing its shield");
+                            case "DEAD" -> System.out.println(robotName + " is dead");
+                            case "Could not parse arguments" -> System.out.println("could not parse arguments");
+                            default -> System.out.println(robotName+ ": turned " + instruction[1]  + " - now facing "+ serverResponse.path("state").path("direction").asText());
+                        }
+                    }
+                    case ("look") -> {
+
+                        if (serverResponse.path("result").asText().equals("ERROR")) System.out.println(serverResponse.path("data").path("message").asText());
+                        else {
+                            JsonNode data = serverResponse.path("data");
+                            JsonNode status = serverResponse.path("state").path("status");
+                            if (status.asText().equals("DEAD")){
+                                System.out.println(robotName + " is dead");
+                            }else {
+                                System.out.println(robotName + " saw:");
+                                if(data.path("objects").isEmpty()){
+                                    System.out.println(" - Nothing...");
+                                }
+                                for (JsonNode object : data.path("objects")) {
+                                    String article = " - a ";
+                                    if (object.get("type").asText().equals("EDGE")) {
+                                        article = " - the ";
+                                    }
+                                    System.out.println(article + object.get("type").asText().toLowerCase() + ", " + object.get("distance").asText() + " kliks to the " + object.get("direction") + ".");
+                                }
+                            }
+                        }
+                    }
+                    case ("state") -> {
+                        if (serverResponse.path("result").asText().equals("ERROR")) System.out.println(serverResponse.path("data").path("message").asText());
+                        else {
+                            JsonNode state = serverResponse.path("state");
+                            System.out.println("Robot State:");
+                            System.out.println("Position: " + state.path("position"));
+                            System.out.println("Direction: " + state.path("direction"));
+                            System.out.println("Shields: " + state.path("shields"));
+                            System.out.println("Shots: " + state.path("shots"));
+                            System.out.println("Status: " + state.path("status"));
+                        }
+                    }
+                    case ("fire") -> {
+                        if (!serverResponse.path("result").asText().equals("ERROR")) {
+                            JsonNode data = serverResponse.path("data");
+                            if (data.path("message").asText().equals("No more shots.")) {
+                                System.out.println("No shots left.");
+                                continue;
+                            }
+
+                            GraphicsCommand.displayGunGraphic();
+                            if (data.path("message").asText().equals("Hit")) {
+                                System.out.println("Hit another robot!");
+                                System.out.println("Distance: " + data.path("distance").asInt());
+                                System.out.println("Robot: " + data.path("robot").asText());
+                                // Update the number of shots left
+                                int shotsLeft = serverResponse.path("state").path("shots").asInt();
+                                System.out.println("Shots left: " + shotsLeft);
+                            } else if (data.path("message").asText().equals("Miss")) {
+                                System.out.println("Missed!");
+                                // Update the number of shots left
+                                int shotsLeft = serverResponse.path("state").path("shots").asInt();
+                                System.out.println("Shots left: " + shotsLeft);
+                            }
+                        } else {
+                            String message = serverResponse.path("data").path("message").asText();
+                            switch (message){
+                                case "RELOAD" -> System.out.println(robotName + " is currently reloading");
+                                case "REPAIR" -> System.out.println(robotName + " is currently repairing its shield");
+                                case "DEAD" -> System.out.println(robotName + " is dead");
+                                case "Could not parse arguments" -> System.out.println("could not parse arguments");
+                                default -> System.out.println(robotName+ " is reloading.");
+                            }
+                        }
+                    }
 
                 }
             }
