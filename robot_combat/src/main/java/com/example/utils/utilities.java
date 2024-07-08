6d12f694bb06728c1c3d5da4f5add0c091fe3e87
mrampai023 <mrampai023@student.wethinkcode.co.za>
2024-07-08 16:36:23 +0200

    added utilities

---
+++ robot_combat/src/main/java/com/example/utils/utilities.java
@@ -0,0 +1,42 @@
+package com.example.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.swing.text.Utilities;
+
+public class utilities {
+
+    public static boolean isInteger(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isRightOrLeft(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+        return str.equalsIgnoreCase("right") || str.equalsIgnoreCase("left");
+    }
+
+    public static int getIntInput(String prompt) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(prompt);
+        String input = br.readLine();
+        while (!Utilities.isInteger(input)){
+            System.out.println("Please enter an integer.");
+            System.out.println(prompt);
+            input = br.readLine();
+        }
+        return Integer.parseInt(input);
+    }
+    
+}
